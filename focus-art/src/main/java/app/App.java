78e6d18f5bcbb58d16cba30fd24b85e85378d409
anasouzas <anassilva2606@gmail.com>
2022-05-05 22:13:41 -0300

    CRUD de conteudos

--- focus-art/src/main/java/app/App.java
+++ focus-art/src/main/java/app/App.java
@@ -10,8 +10,14 @@ public class App {
 	public static ContentService cs = new ContentService();
     public static void main(String[] args) {
     	staticFiles.location("/public");
+    	port(4567);
         post("/user", (req, res) -> ss.add(req, res));
         post("/main", (req, res) -> ps.login(req, res));
         get("/content/see/:id", (req, res) -> cs.see(req, res));
+        get("/content/check_user_type/:id", (req, res) -> cs.checkUserType(req, res));
+        post("/content/insert", (req, res) -> cs.insert(req, res));
+        get("/content/update/:id", (req, res) -> cs.update(req, res));
+        post("/content/update", (req, res) -> cs.updateSave(req, res));
+        get("/content/delete/:id", (req, res) -> cs.delete(req, res));
     }
 }
---
+++ focus-art/src/main/java/dao/DAO.java
@@ -0,0 +1,45 @@
+package dao;
+import java.sql.*;
+public class DAO {
+	protected Connection conexao;
+	
+	public DAO() {
+		conexao = null;
+	}
+	
+	public boolean conectar() {
+		String driverName = "org.postgresql.Driver";                    
+		String serverName = "localhost";
+		String mydatabase = "Focus";
+		int porta = 5432;
+		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
+		String username = "ti2cc";
+		String password = "ti@cc";
+		boolean status = false;
+
+		try {
+			Class.forName(driverName);
+			conexao = DriverManager.getConnection(url, username, password);
+			status = (conexao == null);
+			System.out.println("Connection with Postgresql was successfull.");
+		} catch (ClassNotFoundException e) { 
+			System.err.println("Connection with Postgresql was not successfull -- Driver not found-- " + e.getMessage());
+		} catch (SQLException e) {
+			System.err.println("Connection with Postgresql was not successfull -- " + e.getMessage());
+		}
+
+		return status;
+	}
+	
+	public boolean close() {
+		boolean status = false;
+		
+		try {
+			conexao.close();
+			status = true;
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		return status;
+	}
+}
