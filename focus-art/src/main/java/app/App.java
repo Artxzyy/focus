890788c7d0235837889cf7540f8f8e203efc10ab
Arthur Rodrigues <arthut.spyk@gmail.com>
2022-05-06 00:43:47 -0300

    update app to remove eclipse modifications that resulted in errors

--- focus-art/src/main/java/app/App.java
+++ focus-art/src/main/java/app/App.java
@@ -12,7 +12,7 @@ public class App {
 	public static ActivityService as = new ActivityService();
     public static void main(String[] args) {
     	staticFiles.location("/public");
-<<<<<<< HEAD
+    	port(4567);
     	get("/content/see/:id", (req, res) -> cs.see(req, res));
     	get("/activity/see/:id", (req, res) -> as.see(req, res));
     	get("/activity/:id", (req, res) -> as.see_all(req, res));
@@ -20,19 +20,11 @@ public class App {
     	post("/create", (req, res) -> as.add(req, res));
     	get("/activity/update/:id", (req, res) -> as.update(req, res));
     	post("/update", (req, res) -> as.change(req, res));
-=======
-    	port(4567);
->>>>>>> branch 'main' of git@github.com:Artxzyy/focus.git
-        post("/user", (req, res) -> ss.add(req, res));
-        post("/main", (req, res) -> ps.login(req, res));
-<<<<<<< HEAD
-=======
         get("/content/see/:id", (req, res) -> cs.see(req, res));
         get("/content/check_user_type/:id", (req, res) -> cs.checkUserType(req, res));
         post("/content/insert", (req, res) -> cs.insert(req, res));
         get("/content/update/:id", (req, res) -> cs.update(req, res));
         post("/content/update", (req, res) -> cs.updateSave(req, res));
         get("/content/delete/:id", (req, res) -> cs.delete(req, res));
->>>>>>> branch 'main' of git@github.com:Artxzyy/focus.git
     }
 }
